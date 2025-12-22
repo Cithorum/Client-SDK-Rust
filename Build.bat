092c91346345639276ba9b46ab9156a43cff7e06
lucas <lucas@cithorum.ca>
2025-12-22 12:53:56 -0600

    Added build script

--- Build.bat
+++ Build.bat
@@ -1,1 +1,12 @@
-call cargo build
+@echo off
+
+rem Set environment to current .bat directory
+pushd "%~dp0"
+
+set CL_EXE="C:\Program Files\LLVM\bin\clang-cl.exe"
+
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=%CL_EXE% -DCMAKE_CXX_COMPILER=%CL_EXE% -S %cd% -B %cd%/Build/Release
+
+rem Build the source code!
+cd Build/Release
+ninja
